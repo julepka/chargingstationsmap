8d85086c6c25bbfd3b0d3eadbd0402d6813851f0
Iuliia Potapenko <julepka@gmail.com>
2016-12-09 17:07:49 -0800

    basic map functionality with user tracking

--- ChargingStationsMap/ChargingStationsMap/ViewController.swift
+++ ChargingStationsMap/ChargingStationsMap/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var centerLocationView: UIView!
+    
+    var userLocation = CLLocationCoordinate2D(latitude: 37.2482536, longitude: -122.014477);
+    
+    @IBAction func centerMap() {
+        mapView.setUserTrackingMode(.follow, animated: true)
+        centerLocationView.isHidden = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let location = mapView.userLocation.location {
+            userLocation = location.coordinate
+        }
+        let region = MKCoordinateRegionMakeWithDistance(userLocation, 2000, 2000)
+        mapView.setRegion(region, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var updateOnLaunch = true
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if updateOnLaunch {
+            if let location = mapView.userLocation.location {
+                self.userLocation = location.coordinate
+                mapView.setUserTrackingMode(.follow, animated: true)
+                updateOnLaunch = false
+            }
+            mapView.centerCoordinate = self.userLocation
+        }
     }
-
+    
+    func mapView(_ mapView: MKMapView, didChange mode: MKUserTrackingMode, animated: Bool) {
+        if mapView.userTrackingMode == .none {
+            centerLocationView.isHidden = false
+        }
+    }
 
 }
 
